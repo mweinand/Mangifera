550cea23af669673da54eb5d9975658600a32f81
Michael Weinand <mike@michaelweinand.com>
2011-05-01 17:15:18 -0500

    added interface for acceleration filter

---
+++ Source/Mangifera/Sensors/Accelerometer/IAccelerationFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mangifera.Sensors.Accelerometer
+{
+    /// <summary>
+    /// An interface to filter acceleration vector data
+    /// </summary>
+    public interface IAccelerationFilter
+    {
+        Vector3D ApplyFilter(Vector3D inputVector);
+    }
+}
